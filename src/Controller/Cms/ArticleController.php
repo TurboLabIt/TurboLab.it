acb7e2f2cfa41a73f41bdcc55bf2e798fda6a445
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-01-13 07:38:43 +0100

    Article controller

---
+++ src/Controller/Cms/ArticleController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller\Cms;
+
+use App\ServiceCollection\Cms\ArticleCollection;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ArticleController extends BaseCmsController
+{
+    public function __construct(protected ArticleCollection $articleCollection)
+    { }
+
+
+    #[Route('/{tagSlugDashId<[^/]+-[1-9]+[0-9]*>}/{articleSlugDashId<[^/]+-[1-9]+[0-9]*>}', name: 'app_article')]
+    public function index(string $tagSlugDashId, string $articleSlugDashId) : Response
+    {
+        $article = $this->articleCollection->loadBySlugDashId($articleSlugDashId);
+
+        $articleRealUrl = $article->checkRealUrl($tagSlugDashId, $articleSlugDashId);
+        if( !empty($articleRealUrl) ) {
+            return $this->redirect($articleRealUrl, Response::HTTP_MOVED_PERMANENTLY);
+        }
+
+        $article->countOneView();
+
+        return $this->render('cms/article/index.html.twig', [
+            'Article' => $article
+        ]);
+    }
+
+
+    #[Route('/{id<[1-9]+[0-9]*>}', name: 'app_article_shorturl')]
+    public function shortUrl(int $id) : Response
+    {
+        $article = $this->articleCollection->loadById($id);
+        return $this->redirect( $article->getUrl(), Response::HTTP_MOVED_PERMANENTLY);
+    }
+}
---
+++ src/Controller/Cms/BaseCmsController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Controller\Cms;
+
+use App\Controller\BaseController;
+
+
+abstract class BaseCmsController extends BaseController
+{ }
