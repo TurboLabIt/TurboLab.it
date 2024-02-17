8f024bdf85910e7b659b1888bf74f5e7406e3acf
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-17 12:41:02 +0100

    Tag pagination tester

--- tests/Smoke/TagTest.php
+++ tests/Smoke/TagTest.php
@@ -7,10 +7,15 @@ use App\Service\Cms\Tag;
 use App\Service\Cms\HtmlProcessor;
 use App\Tests\BaseT;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class TagTest extends BaseT
 {
+    // 👀 https://turbolab.it/turbolab.it-1
+    const SPECIAL_TAG_ID                = 1;
+    const TOTAL_PAGE_NUM_OF_SPECIAL_TAG = 3;
+
     protected static array $arrTagEntity;
 
 
@@ -21,8 +26,8 @@ class TagTest extends BaseT
         /** @var Tag $tag */
         $tag = static::getService("App\\Service\\Cms\\Tag");
         $tag->load(1309);
-
         $url = $tag->getUrl();
+
         $crawler = $this->fetchDomNode($url);
         $this->tagTitleAsH1Checker($tag, $crawler);
     }
@@ -62,15 +67,12 @@ class TagTest extends BaseT
 
     public function testSpecialTag()
     {
-        // 👀 https://turbolab.it/turbolab.it-1
-
         /** @var Tag $tag */
         $tag = static::getService("App\\Service\\Cms\\Tag");
-        $tag->load(1);
-
+        $tag->load(static::SPECIAL_TAG_ID);
         $url = $tag->getUrl();
 
-        $wrongTagUrl = 'http://localhost/wrong-tag-slug-1';
+        $wrongTagUrl = 'http://localhost/wrong-tag-slug-' . static::SPECIAL_TAG_ID;
         $this->expectRedirect($wrongTagUrl, $url);
 
         $crawler = $this->fetchDomNode($url);
@@ -84,11 +86,10 @@ class TagTest extends BaseT
         $countH2 = $H2s->count();
         $this->assertGreaterThan(3, $countH2);
 
-        //
-        $this->internalLinksChecker($crawler);
-
-        //
-        $this->internalImagesChecker($crawler);
+        $this
+            ->internalLinksChecker($crawler)
+            ->internalImagesChecker($crawler)
+            ->internalPaginatorChecker($url, static::TOTAL_PAGE_NUM_OF_SPECIAL_TAG);
     }
 
 
@@ -163,7 +164,7 @@ class TagTest extends BaseT
     }
 
 
-    protected function internalLinksChecker(Crawler $crawler) : void
+    protected function internalLinksChecker(Crawler $crawler) : static
     {
         $aNodes = $crawler->filter('a');
         foreach($aNodes as $a) {
@@ -197,10 +198,12 @@ class TagTest extends BaseT
                 $this->fetchHtml($href);
             }
         }
+
+        return $this;
     }
 
 
-    protected function internalImagesChecker(Crawler $crawler) : void
+    protected function internalImagesChecker(Crawler $crawler) : static
     {
         $imgNodes = $crawler->filter('img');
         foreach($imgNodes as $img) {
@@ -212,5 +215,89 @@ class TagTest extends BaseT
 
             $this->fetchImage($src);
         }
+
+        return $this;
+    }
+
+
+    protected function internalPaginatorChecker(string $url, ?int $expectedTotalPageNum) : static
+    {
+        // first page
+        $basePageUrl    = $url;
+        $crawler        = $this->fetchDomNode($url, 'article');
+        $paginator      = $crawler->filter('div.pagination-container');
+        $paginatorHtml  = mb_strtolower($paginator->html());
+
+        $this->assertStringNotContainsString('precedente', $paginatorHtml);
+        $this->assertStringNotContainsString('prev', $paginatorHtml);
+
+        $this->assertStringContainsString('successiv', $paginatorHtml);
+        $this->assertStringContainsString('next', $paginatorHtml);
+        $nextLink = $paginator->filter('.pagination-next > a');
+        $nextHref = $nextLink->attr('href');
+        $this->assertStringEndsWith('/2', $nextHref);
+
+        // loop on every page but first and last
+        for($i = 2; $i < $expectedTotalPageNum; $i++) {
+
+            // URL of the current page. It will be checked against "prev" on the next page
+            $nextPagePrevLinkUrl = $url;
+
+            // this is the URL of the page to request and test now
+            $url = $nextHref;
+
+            $crawler        = $this->fetchDomNode($url, 'article');
+            $paginator      = $crawler->filter('div.pagination-container');
+            $paginatorHtml  = mb_strtolower($paginator->html());
+
+            // prev checks
+            $this->assertStringContainsString('precedente', $paginatorHtml);
+            $this->assertStringContainsString('prev', $paginatorHtml);
+            $prevLink = $paginator->filter('.pagination-prev > a');
+            $prevHref = $prevLink->attr('href');
+            $this->assertEquals($nextPagePrevLinkUrl, 'http://localhost' . $prevHref);
+
+            // next checks
+            $this->assertStringContainsString('successiv', $paginatorHtml);
+            $this->assertStringContainsString('next', $paginatorHtml);
+            $nextLink = $paginator->filter('.pagination-next > a');
+            $nextHref = $nextLink->attr('href');
+            $this->assertStringEndsWith('/' . $i + 1, $nextHref);
+        }
+
+        // URL of the current page. It will be checked against "prev" on the next page
+        $nextPagePrevLinkUrl = $url;
+
+        // this is the URL of the page to request and test now
+        $lastPageUrl = $nextHref;
+
+        $crawler = $this->fetchDomNode($lastPageUrl, 'article');
+        $paginator = $crawler->filter('div.pagination-container');
+        $paginatorHtml = mb_strtolower($paginator->html());
+
+        // prev checks
+        $this->assertStringContainsString('precedente', $paginatorHtml);
+        $this->assertStringContainsString('prev', $paginatorHtml);
+        $prevLink = $paginator->filter('.pagination-prev > a');
+        $prevHref = $prevLink->attr('href');
+        $this->assertEquals($nextPagePrevLinkUrl, $prevHref);
+
+        // next checks
+        $this->assertStringNotContainsString('successiv', $paginatorHtml);
+
+        // requesting an over-limit page must redirect to the last page
+        foreach([1, 2, 3, 50, 99, 175, 7948] as $lastPageMultiplier)
+        {
+             $overlimitPageNum =
+                 $lastPageMultiplier < 5
+                     ? ( $expectedTotalPageNum + $lastPageMultiplier )
+                     : ( $expectedTotalPageNum * $lastPageMultiplier );
+
+
+            $overlimitUrl = $basePageUrl . "/" . $overlimitPageNum;
+            $this->expectRedirect($overlimitUrl, $lastPageUrl, Response::HTTP_FOUND);
+        }
+
+        return $this;
     }
 }
