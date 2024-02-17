42eb35df9caca21c0a2b090232e3363c2d7c114b
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-17 23:43:47 +0100

    run tli1 import after db-restore

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -4,8 +4,6 @@
 source $(dirname $(readlink -f $0))/script_begin.sh
 
 DAY_NUM=$(date +'%u')
-## temporary, for TLI2 dev while TLI1 is still live
-#DAY_NUM=6
 
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}thundercracker_turbolab_it_${DAY_NUM}.sql.7z
 ## local database name to import into
@@ -14,12 +12,19 @@ MYSQL_DB_NAME=tli1
 SKIP_POST_RESTORE_QUERY=1
 wsuSourceFrameworkScript db-restore
 
-
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}thundercracker_turbolab_it_forum_${DAY_NUM}.sql.7z
 ## local database name to import into
 MYSQL_DB_NAME=turbolab_it_forum
 ## options
 SKIP_POST_RESTORE_QUERY=0
 wsuSourceFrameworkScript db-restore
+
+cd ${PROJECT_DIR}
+
+if [ "$EXPECTED_USER" = "$(whoami)" ]; then
+  symfony console tli1
+else
+  sudo -u "$EXPECTED_USER" -H symfony console tli1
+fi
 
 source "${SCRIPT_DIR}script_end.sh"
