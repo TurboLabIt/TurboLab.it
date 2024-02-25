df9c982fdb02eed2af8167bc97b355539c03aea5
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-26 00:50:01 +0100

    turbolabit/php-encryptor

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    TurboLabIt\Encryptor\TurboLabItEncryptorBundle::class => ['all' => true],
 ];
