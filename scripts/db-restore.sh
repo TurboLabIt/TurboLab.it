16943e62c15f6da5b2d5aea80bd4fba1d3d626f8
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-07-03 06:42:55 +0200

    TLI1 import script https://github.com/TurboLabIt/TurboLab.it/blob/main/docs/tli1-migration.md

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -5,6 +5,7 @@ source $(dirname $(readlink -f $0))/script_begin.sh
 
 DAY_NUM=$(date +'%u')
 
+## WEBSITE
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}thundercracker_turbolab_it_${DAY_NUM}.sql.7z
 ## local database name to import into
 MYSQL_DB_NAME=tli1
@@ -12,11 +13,13 @@ MYSQL_DB_NAME=tli1
 SKIP_POST_RESTORE_QUERY=1
 wsuSourceFrameworkScript db-restore
 
+## FORUM
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}thundercracker_turbolab_it_forum_${DAY_NUM}.sql.7z
 ## local database name to import into
 MYSQL_DB_NAME=turbolab_it_forum
 ## options
 SKIP_POST_RESTORE_QUERY=0
 wsuSourceFrameworkScript db-restore
+
 
 source "${SCRIPT_DIR}script_end.sh"
