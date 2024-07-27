fab79e982b0bd0b834101e0329dfa1b66c7ab080
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-07-27 23:58:18 +0200

    rework contact image + compression via tinypng

--- assets/js/scroll-to-selector.js
+++ assets/js/scroll-to-selector.js
@@ -4,9 +4,6 @@ jQuery(document).on('click', '.tli-scroll-to', function(event){
 
     event.preventDefault();
 
-    let selectorToScrollTo = $(this).data('tli-scroll-to');
-
-    $("html, body").animate({
-        scrollTop: $(selectorToScrollTo).offset().top
-    }, 1000);
+    let selectorToScrollTo = jQuery(this).attr('href');
+    jQuery(selectorToScrollTo).get(0).scrollIntoView({behavior: 'smooth'});
 });
