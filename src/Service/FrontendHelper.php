4c4e8101969b7195208a29d07ede32d6105aca29
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-08-13 09:46:54 +0200

    FrontendHelper-driven navigation (same categories everywhere, yay!)

--- src/Service/FrontendHelper.php
+++ src/Service/FrontendHelper.php
@@ -1,16 +1,26 @@
 <?php
 namespace App\Service;
 
+use App\Service\Cms\Tag;
 use App\ServiceCollection\Cms\ArticleCollection;
+use App\ServiceCollection\Cms\TagCollection;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class FrontendHelper
 {
+    protected ArticleCollection $guidesForAuthors;
+    protected TagCollection $categories;
+
+
     public function __construct(protected UrlGeneratorInterface $urlGenerator, protected Factory $factory) {}
 
 
-    //<editor-fold defaultstate="collapsed" desc="*** Links ***">
+    //<editor-fold defaultstate="collapsed" desc="*** 🔗 Newsletter, Feed, Social links ***">
+    public function getFollowUsLinks() : array
+        { return array_merge( $this->getOwnFollowUsPages(), $this->getSocialMediaPages() ); }
+
+
     public function getOwnFollowUsPages() : array
     {
         return [
@@ -40,28 +50,90 @@ class FrontendHelper
             $this->buildLink("LinkedIn", 'https://linkedin.com/company/turbolabit')
         ];
     }
+    //</editor-fold>
+
+
+    //<editor-fold defaultstate="collapsed" desc="*** 🗂️ Nav menu ***">
+    public function getNavCategories() : array
+        { return array_merge( $this->getNavTopCategories(), $this->getNavOtherCategories() ); }
+
+
+    public function getNavTopCategories() : array
+    {
+        $arrNavItems = $this->buildNavCategories(TagCollection::TOP_CATEGORIES);
+
+        $arrNavItems["news"] =
+            $this->buildLink(
+                "News", $this->urlGenerator->generate('app_news'), false,
+                null, "fa-solid fa-newspaper", "news"
+            );
+
+        return $arrNavItems;
+    }
+
+
+    public function getNavOtherCategories() : array
+        { return $this->buildNavCategories(TagCollection::NAV_OTHER_CATEGORIES); }
+
+
+    protected function buildNavCategories(array $arrCategoryIds) : array
+    {
+        if( empty($this->categories) ) {
+            $this->categories = $this->factory->createTagCollection()->loadCategories();
+        }
+
+        $arrTopCategories = $this->categories->getFilteredData( fn(Tag $tag) => in_array($tag->getId(), $arrCategoryIds) );
+
+        $arrNavItems = [];
+        foreach($arrTopCategories as $tag) {
+
+            $id = (string)$tag->getId();
+            $arrNavItems[$id] =
+                $this->buildLink(
+                    $tag->getTitleFormatted(), $tag->getUrl(), false, null,
+                    $tag->getFontAwesomeIcon(), $tag->getSlug()
+                );
+        }
+
+        return $arrNavItems;
+    }
+    //</editor-fold>
 
 
     protected function buildLink(
         string $name, string $url, bool $blank = true,
-        ?string $iconFileName = null, ?string $faIcon = null
+        ?string $iconFileName = null, null|string|array $faIcon = null,
+        ?string $activeMenu = null
     ) : array
     {
+        if( empty($faIcon) ) {
+            $faIcon = 'fab fa-' . mb_strtolower($name);
+        }
+
+        if( !is_array($faIcon) ) {
+            $faIcon = [$faIcon];
+        }
+
         return [
-            'name'  => $name,
-            'icon'  => $iconFileName ?? ( '/images/social-icons/' . mb_strtolower($name) . '.svg' ),
-            "fa"    => $faIcon ?? ( 'fab fa-' . mb_strtolower($name) ),
-            "url"   => $url,
-            "blank" => $blank
+            'name'          => $name,
+            'icon'          => $iconFileName ?? ( '/images/social-icons/' . mb_strtolower($name) . '.svg' ),
+            "fa"            => $faIcon,
+            "url"           => $url,
+            "blank"         => $blank,
+            "activeMenu"    => $activeMenu,
         ];
     }
     //</editor-fold>
 
 
-    //<editor-fold defaultstate="collapsed" desc="*** Guides for authors ***">
+    //<editor-fold defaultstate="collapsed" desc="*** Articles ***">
     public function getGuidesForAuthors() : ArticleCollection
     {
-        return $this->factory->createArticleCollection()->loadGuidesForAuthors();
+        if( empty($this->guidesForAuthors) ) {
+            $this->guidesForAuthors = $this->factory->createArticleCollection()->loadGuidesForAuthors();
+        }
+
+        return $this->guidesForAuthors;
     }
     //</editor-fold>
 }
