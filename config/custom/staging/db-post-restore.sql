48d40ae7224767e78dca0aa5d05503e1f2430a15
Zane <info@turbolab.it>
2024-08-18 20:49:55 +0200

    fix database name

--- config/custom/staging/db-post-restore.sql
+++ config/custom/staging/db-post-restore.sql
@@ -7,5 +7,5 @@
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/staging/db-post-restore.sql
 
-TRUNCATE turbolab_it_forum.phpbb_sessions_keys;
-TRUNCATE turbolab_it_forum.phpbb_sessions;
+TRUNCATE turbolab_it_next-forum.phpbb_sessions_keys;
+TRUNCATE turbolab_it_next-forum.phpbb_sessions;
