59b37176bd7fc1cddf3243f7e1178c6c3b1dbd96
Zane <info@turbolab.it>
2024-08-18 18:36:42 +0200

    Removing TLI2 assets store var/uploaded-assets

--- scripts/tli1-tli2-hybrid-import.sh
+++ scripts/tli1-tli2-hybrid-import.sh
@@ -36,6 +36,10 @@ bash "${SCRIPT_DIR}phpbb-cli.sh" config:set server_name "next.turbolab.it"
 bash "${SCRIPT_DIR}phpbb-cli.sh" cache:purge
 
 
+fxTitle "Removing TLI2 assets store var/uploaded-assets/..."
+sudo rm -rf ${PROJECT_DIR}var/uploaded-assets
+
+
 fxTitle "Setting up symlinks to TLI1 resources..."
 TLI2_ASSETS_TO_IMPORT_DIR=${PROJECT_DIR}var/uploaded-assets-downloaded-from-remote
 
