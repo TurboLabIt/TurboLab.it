4cfe78915193348b11b008b72eea9d48ed0d8cd5
Zane <info@turbolab.it>
2024-08-18 13:31:27 +0200

    tli1-tli2-hybrid-import.sh

--- scripts/tli1-tli2-hybrid-import.sh
+++ scripts/tli1-tli2-hybrid-import.sh
@@ -8,7 +8,7 @@ fxHeader "Copy data from TLI1 to TLI2 hybrid server"
 source "/etc/turbolab.it/mysql.conf"
 
 fxTitle "Clearing database..."
-mysql -u${MYSQL_USER} -p ${MYSQL_PASSWORD} -H ${MYSQL_HOST} -e "TRUNCATE turbolab_it_next.doctrine_migration_versions"
+mysql -u${MYSQL_USER} -p ${MYSQL_PASSWORD} -H ${MYSQL_HOST} -e "DROP TABLE IF EXISTS turbolab_it_next.doctrine_migration_versions"
 
 bash {$SCRIPT_DIR}migrate.sh
 
