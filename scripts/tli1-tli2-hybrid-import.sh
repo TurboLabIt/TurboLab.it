0aaf2d19cdff8d58f3099c8ae553d24efd23165b
Zane <info@turbolab.it>
2024-08-18 16:42:02 +0200

    CHECKPOINT hybrid import

--- scripts/tli1-tli2-hybrid-import.sh
+++ scripts/tli1-tli2-hybrid-import.sh
@@ -4,27 +4,32 @@ clear
 source $(dirname $(readlink -f $0))/script_begin.sh
 
 fxHeader "Copy data from TLI1 to TLI2 hybrid server"
-
-fxTitle "Dropping doctrine_migration_versions..."
-wsuMysql -e 'DROP TABLE IF EXISTS turbolab_it_next.doctrine_migration_versions'
-
-bash ${SCRIPT_DIR}migrate.sh
-
-fxTitle "Setting up symlinks to TLI1 resources..."
-TLI2_ASSETS_TO_IMPORT_DIR=${PROJECT_DIR}var/uploaded-assets-downloaded-from-remote
-echo "${TLI2_ASSETS_TO_IMPORT_DIR}"
-exit
-
-sudo rm -rf ${TLI2_ASSETS_TO_IMPORT_DIR}
-sudo mkdir -p ${TLI2_ASSETS_TO_IMPORT_DIR}
-cd ${TLI2_ASSETS_TO_IMPORT_DIR}
-
 TLI1_SOURCE_DIR=/var/www/turbolab_it/website/www/
-
-ln -s ${TLI1_SOURCE_DIR}immagini/originali-contenuti images
-ln -s ${TLI1_SOURCE_DIR}files .
+TLI2_FORUM_DIR=${PROJECT_DIR}public/forum/
 
 
-#sudo -u "$EXPECTED_USER" -H symfony console tli1
+fxTitle "Copying the whole forum directory..."
+sudo mv "${TLI2_FORUM_DIR}config.php" "${PROJECT_DIR}backup/phpbb-next-config.php"
+sudo rm -rf "${TLI2_FORUM_DIR}"
+sudo cp -a "${TLI1_SOURCE_DIR}public/forum" "${TLI2_FORUM_DIR%/}"
+sudo rm -f "${TLI2_FORUM_DIR}config.php"
+sudo mv "${PROJECT_DIR}backup/phpbb-next-config.php" "${TLI2_FORUM_DIR}config.php"
 
-#bash "${SCRIPT_DIR}cache-clear.sh"
+if ! grep -q turbolab_it_next_forum "${TLI2_FORUM_DIR}config.php"; then
+  fxCatastrophicError "config.php doesn't contain ##turbolab_it_next_forum##"
+fi
+
+fxTitle "Copying the forum database..."
+bash "${TLI1_SOURCE_DIR}scripts/db-dump.sh"
+cd "${TLI1_SOURCE_DIR}backup/db-dumps"
+zzmysqlimp thundercracker_turbolab_it_forum_$(date +'%u').sql.7z turbolab_it_next_forum
+cd "${PROJECT_DIR}"
+
+
+fxTitle "Changing some forum settings..."
+bash "${SCRIPT_DIR}phpbb-cli.sh" config:set email_enable 0
+bash "${SCRIPT_DIR}phpbb-cli.sh" config:set sitename "next.turbolab.it"
+bash "${SCRIPT_DIR}phpbb-cli.sh" config:set site_home_url "https://next.turbolab.it"
+bash "${SCRIPT_DIR}phpbb-cli.sh" config:set server_name "next.turbolab.it"
+bash "${SCRIPT_DIR}phpbb-cli.sh" cache:purge
+
