36ddfc8a06a1214afb81a427e0f3f9b7afab4923
Zane <info@turbolab.it>
2024-08-23 01:32:33 +0200

    Restore Symfony cron.sh. messenger:consume checked by https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/symfony/cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -5,6 +5,5 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/cron.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-## Disabled (messenger not installed: There are no commands defined in the "messenger" namespace.)
-#wsuSourceFrameworkScript cron "$@"
+wsuSourceFrameworkScript cron "$@"
 source "${SCRIPT_DIR}script_end.sh"
