8f3c63805e41e12801bf2506af1e29af52b63919
Zane <info@turbolab.it>
2024-09-03 08:30:28 +0200

    minor social buttons edit

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,10 @@ import './styles/newspark/stellarnav.css';
 import './styles/newspark/default.css';
 import './styles/newspark/style.css';
 import './styles/app.css';
+import './styles/social-links.css';
 import './styles/glow.css';
 import './styles/footer.css';
+
 import './js/scroll-to-selector'
 import './js/prevent-change-url'
 
