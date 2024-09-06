b9f1fc0a3cc0a1e08ffae0be412bd52dd736aa0a
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-09-07 00:09:41 +0200

    "Info Server" (/info) Closes #39

--- scripts/server-info-dump.sh
+++ scripts/server-info-dump.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-
 fxHeader 'Dump server info'
+
 
 fxTitle "Output directory..."
 DUMP_DIR=${PROJECT_DIR}var/server-info-dump/
 fxMessage "##${DUMP_DIR}##"
+sudo rm -rf "${DUMP_DIR}"
+sudo -u ${EXPECTED_USER} mkdir -p "${DUMP_DIR}"
 
-if [ ! -d "${DUMP_DIR}" ]; then
-
-    mkdir -p "${DUMP_DIR}"
-    fxOK "Created!"
-
-else
-
-    fxOK "Directory exists"
-fi
 
 fxTitle "lsb_release..."
-lsb_release -a > "${DUMP_DIR}lsb_release.txt" 2>&1
+sudo -u ${EXPECTED_USER} lsb_release -a > "${DUMP_DIR}lsb_release.txt" 2>&1
+
 
 fxTitle "git commit..."
-git rev-parse --short HEAD > "${DUMP_DIR}git-commit.txt" 2>&1
+sudo -u ${EXPECTED_USER} git rev-parse --short HEAD > "${DUMP_DIR}git-commit.txt" 2>&1
+
 
 source "${SCRIPT_DIR}/script_end.sh"
