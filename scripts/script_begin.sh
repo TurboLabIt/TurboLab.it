9ca744ecb65bba955b3d4238e09e26318500ca76
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-10-26 07:02:57 +0200

    deploy script

--- scripts/script_begin.sh
+++ scripts/script_begin.sh
@@ -3,7 +3,7 @@
 
 APP_NAME="turbolab.it"
 PROJECT_FRAMEWORK=symfony
-ZZ_CMD_SUFFIX=1
+LETS_ENCRYPT_SKIP_RENEW=1
 
 ## https://github.com/TurboLabIt/webstackup/blob/master/script/filesystem/script_begin_start.sh
 source "/usr/local/turbolab.it/webstackup/script/filesystem/script_begin_start.sh"
@@ -12,10 +12,12 @@ source "/usr/local/turbolab.it/webstackup/script/filesystem/script_begin_start.s
 if [ "$APP_ENV" = "prod" ]; then
 
   EMOJI=rocket
+  ZZ_CMD_SUFFIX=1
 
 elif [ "$APP_ENV" = "staging" ]; then
 
   APP_NAME="turbolab.it-next"
   DEPLOY_MYSQL_RESTART=0
   EMOJI=cat
+  ZZ_CMD_SUFFIX=0
 fi
