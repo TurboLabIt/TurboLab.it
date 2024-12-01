f82d7b0bbf308c292d51b37110b65604e4c5e6d9
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-12-01 08:28:08 +0100

    fix hardcoded PHP version in dev script

--- scripts/cache-clear.sh
+++ scripts/cache-clear.sh
@@ -10,9 +10,9 @@ if [ "$APP_ENV" = "dev" ]; then
   fxLink ${PROJECT_DIR}config/custom/phpbb-upgrader.conf /etc/turbolab.it/phpbb-upgrader-turbolab.it.conf
   fxLink ${PROJECT_DIR}config/custom/zzmysqldump.conf /etc/turbolab.it/zzmysqldump.profile.turbolab.it.conf
 
-  fxLink ${PROJECT_DIR}config/custom/php-custom.ini /etc/php/8.3/fpm/conf.d/90-turbolab.it.ini
-  fxLink ${PROJECT_DIR}config/custom/php-custom.ini /etc/php/8.3/cli/conf.d/90-turbolab.it.ini
-  fxLink ${PROJECT_DIR}config/custom/php-custom-cli.ini /etc/php/8.3/cli/conf.d/95-turbolab.it-cli.ini
+  fxLink ${PROJECT_DIR}config/custom/php-custom.ini /etc/php/${PHP_VER}/fpm/conf.d/90-turbolab.it.ini
+  fxLink ${PROJECT_DIR}config/custom/php-custom.ini /etc/php/${PHP_VER}/cli/conf.d/90-turbolab.it.ini
+  fxLink ${PROJECT_DIR}config/custom/php-custom-cli.ini /etc/php/${PHP_VER}/cli/conf.d/95-turbolab.it-cli.ini
 
   fxLink ${PROJECT_DIR}config/custom/dev/nginx-dev0.conf /etc/nginx/conf.d/turbolab.it-dev0.conf
 
