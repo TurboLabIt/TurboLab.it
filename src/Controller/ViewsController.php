d838bb41cda43ac76e4a822bd9106370f30738af
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-12-29 18:56:44 +0100

    Implementare le "viste" sugli articoli #23 https://github.com/TurboLabIt/TurboLab.it/issues/23

--- src/Controller/ViewsController.php
+++ src/Controller/ViewsController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\Cache\CacheItem;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 
@@ -11,5 +13,18 @@ class ViewsController extends BaseController
 
 
     #[Route('/' . self::SECTION_SLUG . '/{slug}/{page<[0-9]*>}', name: 'app_views_legacy', requirements: ['slug' => 'tutti'])]
-    public function legacy(?int $page = 1) : Response { return $this->redirectToRoute('app_home', [], Response::HTTP_MOVED_PERMANENTLY); }
+    public function legacy($page = 1) : Response
+        { return $this->redirectToRoute('app_home', [], Response::HTTP_MOVED_PERMANENTLY); }
+
+
+    #[Route('/' . self::SECTION_SLUG . '/{slug}/{page<0|1>}', name: 'app_views_multi_0-1')]
+    public function appViewsMulti0Or1($slug) : Response
+        { return $this->redirectToRoute('app_views_multi', ["slug" => $slug], Response::HTTP_MOVED_PERMANENTLY); }
+
+
+    #[Route('/' . self::SECTION_SLUG . '/{slug}', name: 'app_views_multi')]
+    public function multi($slug) : Response
+    {
+        return new Response('OK');
+    }
 }
--- tests/Smoke/ViewsTest.php
+++ tests/Smoke/ViewsTest.php
@@ -8,13 +8,39 @@ use PHPUnit\Framework\Attributes\DataProvider;
 
 class ViewsTest extends BaseT
 {
-    public static function legacyViewsUrlProvider() : Generator
+    public static function viewSlugProvider() : Generator
+    {
+        yield from [['bozze'], ['finiti']];
+    }
+
+
+    #[DataProvider('viewSlugProvider')]
+    public function testPagination0or1RedirectsToView(string $slug)
+    {
+        foreach([0, 1] as $pageNum) {
+
+            $url = $this->generateUrl('app_views_multi', ['slug' => $slug]) . "/$pageNum";
+            $expectedUrl = $this->generateUrl('app_views_multi', ['slug' => $slug]);
+            $this->expectRedirect($url, $expectedUrl);
+        }
+    }
+
+
+    #[DataProvider('viewSlugProvider')]
+    public function testViewOK(string $slug)
+    {
+        $url = $this->generateUrl('app_views_multi', ['slug' => $slug]);
+        $this->fetchHtml($url);
+    }
+
+
+    public static function legacyViewSlugProvider() : Generator
     {
         yield from [['tutti']];
     }
 
 
-    #[DataProvider('legacyViewsUrlProvider')]
+    #[DataProvider('legacyViewSlugProvider')]
     public function testLegacyViewRedirect(string $slug)
     {
         $url = $this->generateUrl('app_views_legacy', ['slug' => $slug]);
@@ -22,7 +48,7 @@ class ViewsTest extends BaseT
 
         foreach([null, 0, 1, 2, 3, 10, 11, 99, 247] as $pageNum) {
 
-            $url = $this->generateUrl('app_views_legacy', ['slug' => $slug, 'page' => $pageNum]);
+            $url = $this->generateUrl('app_views_legacy', ['slug' => $slug]) . "/$pageNum";
             $this->expectRedirect($url, '/');
         }
     }
