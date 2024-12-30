2665e9f93c8f8ea11322a8516571b9eca816f5a0
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-12-30 23:40:23 +0100

    Implementare le "viste" sugli articoli https://github.com/TurboLabIt/TurboLab.it/issues/23 Closes #23

--- src/Controller/ViewsController.php
+++ src/Controller/ViewsController.php
@@ -10,7 +10,7 @@ use TurboLabIt\PaginatorBundle\Exception\PaginatorOverflowException;
 
 class ViewsController extends BaseController
 {
-    const string SECTION_SLUG = "viste";
+    const string SECTION_SLUG   = "viste";
 
 
     #[Route('/' . self::SECTION_SLUG . '/{slug}/{page<0|1>}', name: 'app_views_multi_0-1')]
@@ -21,18 +21,33 @@ class ViewsController extends BaseController
     #[Route('/' . self::SECTION_SLUG . '/{slug}/{page<[1-9]+[0-9]*>}', name: 'app_views_multi')]
     public function multi(string $slug, ?int $page = null) : Response
     {
-        $fxLoadArticle =
-            match($slug) {
-                "bozze"     => 'loadDrafts',
-                "finiti"    => 'loadLatestReadyForReview',
-                "visitati"  => 'loadTopViews',
-                "commentati"=> 'loadTopTopComments',
-                default     => throw new NotFoundHttpException()
-            };
+        $arrDefinedViews = [
+            "bozze"         => [
+                "title" => "Articoli non ancora completati (bozze)",
+                "fx"    => 'loadDrafts'
+            ],
+            "finiti"        => [
+                "title" => "Articoli finiti, in attesa di pubblicazione",
+                "fx"    => 'loadLatestReadyForReview'
+            ],
+            "visitati"      => [
+                "title" => "Gli articoli più visitati",
+                "fx"    => 'loadTopViews'
+            ],
+            "commentati"    => [
+                "title" => "Gli articoli più commentati",
+                "fx"    => 'loadTopTopComments'
+            ]
+        ];
+
+        $arrViewRequested = $arrDefinedViews[$slug] ?? null;
+        if( empty($arrViewRequested) ) {
+            throw new NotFoundHttpException();
+        }
 
         if( !$this->isCachable() ) {
 
-            $buildHtmlResult = $this->buildHtml($slug, $fxLoadArticle, $page);
+            $buildHtmlResult = $this->buildHtml($slug, $arrViewRequested, $page);
             return is_string($buildHtmlResult) ? new Response($buildHtmlResult) : $buildHtmlResult;
         }
 
@@ -40,9 +55,9 @@ class ViewsController extends BaseController
         $that = $this;
 
         $buildHtmlResult =
-            $this->cache->get($cacheKey, function(CacheItem $cache) use($that, $slug, $fxLoadArticle, $page, $cacheKey) {
+            $this->cache->get($cacheKey, function(CacheItem $cache) use($that, $slug, $arrViewRequested, $page, $cacheKey) {
 
-                $buildHtmlResult = $that->buildHtml($slug, $fxLoadArticle, $page);
+                $buildHtmlResult = $that->buildHtml($slug, $arrViewRequested, $page);
 
                 if( is_string($buildHtmlResult) ) {
 
@@ -61,10 +76,12 @@ class ViewsController extends BaseController
     }
 
 
-    protected function buildHtml(string $slug, string $fxLoadArticle, ?int $page = null) : Response|string
+    protected function buildHtml(string $slug, array $arrViewRequested, ?int $page = null) : Response|string
     {
         $baseUrl    = $this->generateUrl('app_views_multi', ["slug" => $slug]);
-        $articles   = $this->factory->createArticleCollection()->$fxLoadArticle($page);
+
+        $fxLoad     = $arrViewRequested["fx"];
+        $articles   = $this->factory->createArticleCollection()->$fxLoad($page);
 
         try {
             $oPages =
@@ -80,7 +97,8 @@ class ViewsController extends BaseController
         }
 
         $arrData = [
-            'metaTitle'         => "Vista $slug",
+            'metaTitle'         => $arrViewRequested["title"] . ( $page < 2 ? '' : " - Pagina $page"),
+            'breadcrumbText'    => $arrViewRequested["title"],
             'metaCanonicalUrl'  => $baseUrl . ( empty($page) || $page <= 1 ? '' : "/$page" ),
             'activeMenu'        => null,
             'FrontendHelper'    => $this->frontendHelper,
