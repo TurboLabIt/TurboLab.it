ead58870893158f6cd63bdba6ebc6c8ef99742ea
Zane <info@turbolab.it>
2025-03-08 17:49:51 +0100

    fix dev db name

--- config/custom/dev/db-post-restore.sql
+++ config/custom/dev/db-post-restore.sql
@@ -7,6 +7,6 @@
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/dev/db-post-restore.sql
 
-USE tli1_forum;
+USE turbolab_it_forum;
 UPDATE phpbb_config SET config_value = 'dev0.turbolab.it' WHERE config_name = 'server_name';
 UPDATE phpbb_config SET config_value = 'https://dev0.turbolab.it' WHERE config_value = 'https://turbolab.it';
