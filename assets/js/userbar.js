300405062ffe740b338e72949e0e7ccfa16ec28c
Zane <info@turbolab.it>
2025-06-12 01:44:37 +0200

    trivial

--- assets/js/userbar.js
+++ assets/js/userbar.js
@@ -3,8 +3,7 @@
 let userBarTarget       = jQuery('#tli-userbar-load-target');
 let userbarLoadingUrl   = userBarTarget.data('loading-url');
 
-jQuery('#tli-userbar-load-target').load(
-    userbarLoadingUrl, { originUrl: window.location.href }, function() {
+userBarTarget.load(userbarLoadingUrl, { originUrl: window.location.href }, function() {
 
     let userbar = jQuery('#tli-userbar');
     if( userbar.hasClass('user-anonymous') ) {
