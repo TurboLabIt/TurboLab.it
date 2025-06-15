7aa6201be434d1e17a2252de5a8ba9e521fb9a54
Zane <info@turbolab.it>
2025-06-15 12:15:25 +0200

    test suite fix: The "App\Service\HtmlProcessorForDisplay" service or alias has been removed or inlined when the container was compiled.

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -7,6 +7,8 @@ use App\Service\Cms\File;
 use App\Service\Cms\Image;
 use App\Service\Cms\Tag;
 use App\Service\FrontendHelper;
+use App\Service\HtmlProcessorForDisplay;
+use App\Service\HtmlProcessorForStorage;
 use App\Service\Views;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -37,7 +39,9 @@ class TestController extends BaseController
     #[Route('/' . self::SECTION_SLUG . '/prevent-unused-service/', name: 'app_test_prevent-unused-service', condition: "'%kernel.environment%' == 'dev'")]
     public function preventUnusedService(
         Article $article, ArticleEditor $articleEditor,
-        File $file, Image $image, Tag $tag, Views $views
+        File $file, Image $image, Tag $tag, Views $views,
+        HtmlProcessorForDisplay $htmlProcessorForDisplay,
+        HtmlProcessorForStorage $htmlProcessorForStorage
     ) : Response
     {
         dd("OK");
