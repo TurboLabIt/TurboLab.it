436de0b3bb878eb5e11761b7321e01ad40d59b7b
Zane <info@turbolab.it>
2025-06-21 01:02:02 +0200

    HTML_QUOTES constant

--- src/Service/Dictionary.php
+++ src/Service/Dictionary.php
@@ -7,6 +7,12 @@ class Dictionary
     // https://github.com/TurboLabIt/TurboLab.it/blob/main/docs/encoding.md
     const array HTML_SPECIAL_CHARS = ['&', '<', '>', '"', "'"];
 
+    const array HTML_QUOTES = [
+      '&quote;' => '"',
+      '&#39;'   => "'",
+      '&apos;'  => "'",
+    ];
+
     // https://www.w3.org/wiki/Common_HTML_entities_used_for_typography
     const array FINE_TYPOGRAPHY_CHARS = [
         // non-breaking space
