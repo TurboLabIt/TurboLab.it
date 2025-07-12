e95a3d8bc3e073f31023925f640a76052d7a4df8
Zane <info@turbolab.it>
2025-07-13 01:49:18 +0200

    SKIP_POST_RESTORE_CACHE_CLEAR in db-restore

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -5,22 +5,20 @@ source $(dirname $(readlink -f $0))/script_begin.sh
 
 DAY_NUM=$(date +'%u')
 
+SKIP_POST_RESTORE_CACHE_CLEAR=1
+
 ## WEBSITE
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}thundercracker_turbolab_it_v1_${DAY_NUM}.sql.7z
 ## local database name to import into
 MYSQL_DB_NAME=tli1
-## options
 SKIP_POST_RESTORE_QUERY=1
-SKIP_POST_RESTORE_CACHE_CLEAR=1
 wsuSourceFrameworkScript db-restore
 
 ## FORUM
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}thundercracker_turbolab_it_forum_${DAY_NUM}.sql.7z
 ## local database name to import into
 MYSQL_DB_NAME=turbolab_it_forum
-## options
 SKIP_POST_RESTORE_QUERY=0
-SKIP_POST_RESTORE_CACHE_CLEAR=0
 wsuSourceFrameworkScript db-restore
 
 
