1cbda6ef724a0663a0180489536b0959c8ed9425
Zane <info@turbolab.it>
2025-07-21 12:00:23 +0200

    ArticleEdit split

--- src/Controller/Editor/ArticleEditAuthors.php
+++ src/Controller/Editor/ArticleEditAuthors.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller\Editor;
 
+use Error;
+use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
