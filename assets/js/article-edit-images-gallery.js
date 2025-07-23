7c2f296b1a2c5e29b0331bff699a8adc24298801
Zane <info@turbolab.it>
2025-07-23 21:09:33 +0200

    Article editor: images gallery

--- assets/js/article-edit-images-gallery.js
+++ assets/js/article-edit-images-gallery.js
@@ -63,6 +63,15 @@ $(document).on('change', '#tli-article-editor-image-gallery input[type="file"]',
 
             $('#tli-images-gallery').append(response);
 
+            const container = $('#tli-article-editor-image-gallery');
+            const target = container.find('.border-success');
+
+            if(target.length) {
+                container.animate({
+                    scrollTop: target.offset().top - container.offset().top + container.scrollTop()
+                }, 1000);
+            }
+
             editorImageGallery.find('.tli-no-images-guide').fadeOut('slow', function(){
                 editorImageGallery.find('.tli-images-guide').fadeIn('fast');
             });
@@ -89,4 +98,63 @@ $(document).on('change', '#tli-article-editor-image-gallery input[type="file"]',
             });
         }
     });
+});
+
+
+/**
+ * IMAGE INSERT LOGIC
+ * ------------------
+ */
+
+let savedRange;
+const editableDiv = $('.tli-article-body');
+
+// saves the current selection (cursor position) within the editable div.
+const saveSelection = () => {
+    // Check if the selection is inside the editable div
+    const selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        if (editableDiv[0].contains(range.commonAncestorContainer)) {
+            savedRange = range;
+        }
+    }
+};
+
+editableDiv.on('keyup mouseup focus', saveSelection);
+
+
+$(document).on('click', '#tli-images-gallery img', function(e) {
+
+    e.preventDefault();
+
+    let image = $(this).clone();
+    image
+        .removeAttr('class')
+        .removeAttr('alt')
+        .removeAttr('title')
+        .removeAttr('loading');
+
+    let imageNode = $('<p>').append(image)[0];
+
+    editableDiv.focus();
+
+    if (savedRange) {
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(savedRange); // Restore the saved cursor position
+
+        // Insert the image at the restored cursor position
+        savedRange.insertNode(imageNode);
+
+        // Move the cursor to be right after the newly inserted image
+        savedRange.setStartAfter(imageNode);
+        savedRange.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(savedRange);
+
+    } else {
+        // Fallback: if no cursor position was ever saved, just append the image
+        editableDiv.append(imageNode);
+    }
 });
