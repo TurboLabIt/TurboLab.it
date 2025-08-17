8e84f0f2a6c0b7f40b03137e9611bf94d95419cf
Zane <info@turbolab.it>
2025-08-17 17:11:21 +0200

    strip quotes from "<code>....</code>"

--- src/Service/HtmlProcessorForStorage.php
+++ src/Service/HtmlProcessorForStorage.php
@@ -67,6 +67,9 @@ class HtmlProcessorForStorage extends HtmlProcessorBase
             '<i>'               => '<em>',
             '</i>'              => '</em>',
 
+            '"<code>'               => '<code>',
+            '</code>"'              => '</code>',
+
             '<p><p>'            => '<p>',
             '</p></p>'          => '</p>',
 
