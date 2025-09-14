0d1089bf8cc6ad71bb4ce8f1ac5d178e9785a1ac
Zane <info@turbolab.it>
2025-09-14 14:45:43 +0200

    remove dead code (staging uses tli1-download!!)

--- scripts/tli1-tli2-hybrid-import.sh
+++ scripts/tli1-tli2-hybrid-import.sh
@@ -9,42 +9,7 @@ TLI1_SOURCE_DIR=/var/www/turbolab_it/website/www/
 
 
 bash "${TLI1_SOURCE_DIR}scripts/db-dump.sh"
-
-
-if [ "$APP_ENV" == "staging" ]; then
-
-  TLI2_DATABASE_NAME=turbolab_it_next
-  TLI2_FORUM_DIR=${PROJECT_DIR}public/forum/
-
-  fxTitle "Copying the whole forum directory..."
-  sudo mv "${TLI2_FORUM_DIR}config.php" "${PROJECT_DIR}backup/phpbb-next-config.php"
-  sudo rm -rf "${TLI2_FORUM_DIR}"
-  sudo cp -rL --preserve "${TLI1_SOURCE_DIR}public/forum" "${TLI2_FORUM_DIR%/}"
-  sudo rm -f "${TLI2_FORUM_DIR}config.php"
-  sudo mv "${PROJECT_DIR}backup/phpbb-next-config.php" "${TLI2_FORUM_DIR}config.php"
-  sudo chown webstackup:www-data "${TLI2_FORUM_DIR}" -R
-
-  if ! grep -q turbolab_it_next_forum "${TLI2_FORUM_DIR}config.php"; then
-    fxCatastrophicError "config.php doesn't contain ##turbolab_it_next_forum##"
-  fi
-
-  cd "${TLI1_SOURCE_DIR}backup/db-dumps"
-  zzmysqlimp "$(hostname)_turbolab_it_forum_$(date +'%u').sql.7z" turbolab_it_next_forum
-
-  fxTitle "Changing some forum settings..."
-  bash "${SCRIPT_DIR}phpbb-cli.sh" config:set email_enable 0
-  bash "${SCRIPT_DIR}phpbb-cli.sh" config:set smtp_host "null.turbolab.it"
-  bash "${SCRIPT_DIR}phpbb-cli.sh" config:set sitename "next.turbolab.it"
-  bash "${SCRIPT_DIR}phpbb-cli.sh" config:set site_home_url "https://next.turbolab.it"
-  bash "${SCRIPT_DIR}phpbb-cli.sh" config:set server_name "next.turbolab.it"
-  bash "${SCRIPT_DIR}phpbb-cli.sh" cache:purge
-
-elif [ "$APP_ENV" == "prod" ]; then
-
-  TLI2_DATABASE_NAME=turbolab_it
-fi
-
-
+TLI2_DATABASE_NAME=turbolab_it
 cd "${TLI1_SOURCE_DIR}backup/db-dumps"
 zzmysqlimp "$(hostname)_turbolab_it_v1_$(date +'%u').sql.7z" turbolab_it_next_tli1_to_import
 
