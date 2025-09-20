1084bb361ae016e2572a9c2acac8eef3d443388e
Zane <info@turbolab.it>
2025-09-20 11:33:56 +0200

    fxSetBackgroundColor

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -1,3 +1,23 @@
 ## this should be sourced from .bashrc
 
+# background color per-host
+if [[ $- == *i* && -n "$SSH_CONNECTION" ]]; then
+
+    case "$(hostname -s)" in
+      turbolab.it)
+        # very dark red
+        fxSetBackgroundColor "#F2F2F2" "#240000"
+        ;;
+      next)
+        # very dark orange
+        fxSetBackgroundColor "#F2F2F2" "#241600"
+        ;;
+    esac
+
+  # Ensure colors go back to default when the shell exits
+  trap fxResetBackgroundColor EXIT
+
+fi
+
+
 cd /var/www/turbolab.it
