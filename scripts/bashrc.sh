160ffa8d285610eca1975afc9af44b77d0690151
Zane <info@turbolab.it>
2025-09-23 18:28:09 +0200

    update bashrc.sh to use fxSetBackgroundColorByHostAndEnv

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -1,25 +1,23 @@
-## this should be sourced from .bashrc
+## 🚨 WARNING 🚨
+#
+# This file is under version control!
+# DO NOT EDIT DIRECTLY - If you do, you'll loose your changes!
+#
+# The original file is in `/var/www/turbolab.it/scripts/`
+#
+# You MUST:
+#
+# 1. edit the original file on you PC
+# 2. Git-commit+push the changes
+# 3. run `sudo bash /var/www/turbolab.it/scripts/deploy.sh`
+#
+# ⚠️ This file is SHARED among dev|staging|prod ⚠️
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/bashrc.sh
 
-# background color per-host
-if [[ $- == *i* && -n "$SSH_CONNECTION" ]]; then
-
-    source "/usr/local/turbolab.it/bash-fx/scripts/colors.sh"
-
-    case "$(hostname -s)" in
-      thundercracker)
-        # very dark red
-        fxSetBackgroundColor "#F2F2F2" "#240000"
-        ;;
-      next-tli)
-        # very dark orange
-        fxSetBackgroundColor "#F2F2F2" "#241600"
-        ;;
-    esac
-
-  # Ensure colors go back to default when the shell exits
-  trap fxResetBackgroundColor EXIT
-
-fi
-
+source "/usr/local/turbolab.it/bash-fx/scripts/colors.sh"
+fxSetBackgroundColorByHostAndEnv "thundercracker" "prod"
+fxSetBackgroundColorByHostAndEnv "next-tli" "next"
+trap fxResetBackgroundColor EXIT
 
 cd /var/www/turbolab.it
