c4f4570e35657c45cad7219e8ddac2a9751e382e
Zane <info@turbolab.it>
2025-11-30 18:35:29 +0100

    download: final clenup -- allegare agli articoli https://github.com/TurboLabIt/TurboLab.it/issues/95 Closes #95

--- assets/js/files-edit.js
+++ assets/js/files-edit.js
@@ -20,64 +20,14 @@ $(document).on('change', 'input[type="file"].tli-file-uploader', function() {
 
     let thisInputFile = $(this);
 
-    /*
-    let editorFileGallery = $('#tli-article-editor-file-gallery');
-
-    const uploadCommandContainer = $('.tli-file-upload');
-    uploadCommandContainer.addClass('d-none');
-
-    const progressBarContainer = $('#tli-file-upload-progress');
-    progressBarContainer.removeClass('collapse');
-
-    const progressBar = progressBarContainer.find('.progress-bar');
-    progressBar.addClass('progress-bar-animated progress-bar-striped');
-    progressBar.removeClass('bg-success bg-danger');
-    progressBar.width('0%');
-    progressBar.attr('aria-valuenow', 0).text('0%');
-
-    let errorMessage = editorFileGallery.find('.alert-danger');
-    errorMessage.addClass('collapse');
-
-    editorFileGallery.find('.border-success').removeClass('border border-2 border-success');
-    */
-
     $.ajax({
-        //url: editorFileGallery.data('save-url'),
         url: thisInputFile.data('save-url'),
         type: 'POST',
         data: formData,
         processData: false,
         contentType: false,
-        /*xhr: function() {
-            const xhr = new window.XMLHttpRequest();
-            // Upload progress
-            xhr.upload.addEventListener('progress', function(evt) {
-                if (evt.lengthComputable) {
-                    const percentComplete = Math.round((evt.loaded / evt.total) * 100);
-                    progressBar.width(percentComplete + '%');
-                    progressBar.attr('aria-valuenow', percentComplete).text(percentComplete + '%');
-                }
-            }, false);
-            return xhr;
-        },*/
         success: function(response) {
 
-            /*progressBar.addClass('bg-success');
-
-            $('#tli-files-gallery').append(response);
-
-            const container = $('#tli-article-editor-file-gallery');
-            const target = container.find('.border-success');
-
-            if(target.length) {
-                container.animate({
-                    scrollTop: target.offset().top - container.offset().top + container.scrollTop()
-                }, 1000);
-            }
-
-            editorFileGallery.find('.tli-no-files-guide').fadeOut('slow', function(){
-                editorFileGallery.find('.tli-files-guide').fadeIn('fast');
-            });*/
             let target = $('#tli-downloadable-files');
             target.fadeOut('slow', function() {
                 target
@@ -89,34 +39,15 @@ $(document).on('change', 'input[type="file"].tli-file-uploader', function() {
         },
         error: function(jqXHR, textStatus, errorThrown) {
 
-            /*progressBar.addClass('bg-danger');
-
-            errorMessage
-                .removeClass('collapse')
-                .html(jqXHR.responseText);*/
             alert(jqXHR.responseText);
         },
         complete: function() {
 
             thisInputFile.val('');
-            /*progressBar.removeClass('progress-bar-animated progress-bar-striped');
-
-            progressBarContainer.delay(3000).fadeOut('slow', function(){
-
-                $(this).addClass('collapse');
-                $(this).css('display', '');
-                uploadCommandContainer.removeClass('d-none');
-            });*/
         }
     });
 });
 
-
-/**
- * FILE INSERT LOGIC
- * ------------------
- * **TODO** --moved to assets/js/article-edit-ckeditor.js--
- */
 
 $(document).on('click', '.tli-file-button-ok',  function(event) {
 
@@ -159,11 +90,12 @@ $(document).on('click', '#tli-downloadable-files .tli-delete-file', function(e) 
         return false;
     }
 
-    let fileContainer = $(this).closest('[data-detach-from-article-url]');
-    fileContainer.removeClass('d-flex');
-    fileContainer.fadeOut();
+    let fileContainer = $(this).closest('.tli-file-download');
+    fileContainer
+        .removeClass('d-flex')
+        .fadeOut();
 
-    let deleteUrl = fileContainer.data('detach-from-article-url');
+    let deleteUrl = $(this).closest('[data-detach-from-article-url]').data('detach-from-article-url');
 
     $.ajax({
         url: deleteUrl,
