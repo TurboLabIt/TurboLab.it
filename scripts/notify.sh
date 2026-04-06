a47ca9d49e2ca7739851ef41498ff33b33ecdef1
Zane <info@turbolab.it>
2026-04-06 12:13:39 +0200

    notify.sh isn't configured, shouldn't run at all

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/notify.sh
 
-source $(dirname $(readlink -f $0))/script_begin.sh
+#source $(dirname $(readlink -f $0))/script_begin.sh
 #source "${PRIVGEN_DIR}operations/webstackup/notify.sh"
-source $(dirname $(readlink -f $0))/script_end.sh
+#source $(dirname $(readlink -f $0))/script_end.sh
