879ecaf84d668ada16ee3b3c5d521bb08a288e84
Zane <info@turbolab.it>
2026-07-18 00:01:10 +0200

    Deprecation fix: Since symfony/framework-bundle 8.1: Setting the "framework.profiler.collect_serializer_data" configuration option is deprecated. It will be removed in version 9.0.

--- src/DataCollector/WebDebugToolbarExtension.php
+++ src/DataCollector/WebDebugToolbarExtension.php
@@ -7,21 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 
-// 📚 https://symfony.com/doc/current/profiler.html#creating-a-data-collector
+/**
+ * Not a real data collector: it collects nothing. It exists only to inject a
+ * new toolbar item (handy dev links) into the web debug toolbar.
+ * This is the officially supported way to add a toolbar item.
+ * 📚 https://symfony.com/doc/current/profiler.html#creating-a-data-collector
+ */
 class WebDebugToolbarExtension extends AbstractDataCollector
 {
     public function collect(Request $request, Response $response, ?Throwable $exception = null) : void
     {
-        // Collect the data that you want to display in the toolbar
-        $this->data = [
-            'custom_value' => 'Your custom value',
-        ];
-    }
-
-
-    public function getCustomValue() : string
-    {
-        return $this->data['custom_value'];
+        // Nothing to collect: the toolbar item is a static template.
     }
 
     public static function getTemplate() : ?string { return 'dev/web-debug-toolbar-extension.html.twig'; }
