73bc2182331abcfb9f2dd54e490750510179e2ee
Zane <info@turbolab.it>
2026-07-23 18:18:53 +0200

    restore SMTP=localhost on import

--- config/custom/dev/db-post-restore.sql
+++ config/custom/dev/db-post-restore.sql
@@ -10,8 +10,11 @@
 USE turbolab_it_forum;
 UPDATE phpbb_config SET config_value = 'dev0.turbolab.it' WHERE config_name = 'server_name';
 UPDATE phpbb_config SET config_value = 'https://dev0.turbolab.it' WHERE config_value = 'https://turbolab.it';
+
+UPDATE phpbb_config SET config_value = 'localhost' WHERE config_name = 'smtp_host';
+UPDATE phpbb_config SET config_value = 25 WHERE config_name = 'smtp_port';
+UPDATE phpbb_config SET config_value = '' WHERE config_name = 'smtp_username';
+UPDATE phpbb_config SET config_value = '' WHERE config_name = 'smtp_password';
+
 TRUNCATE phpbb_sessions_keys;
 TRUNCATE phpbb_sessions;
-#TRUNCATE phpbb_search_wordlist;
-#TRUNCATE phpbb_search_wordmatch;
-#TRUNCATE phpbb_search_results;
--- config/custom/staging/db-post-restore.sql
+++ config/custom/staging/db-post-restore.sql
@@ -10,5 +10,11 @@
 USE turbolab_it_forum;
 UPDATE phpbb_config SET config_value = 'next.turbolab.it' WHERE config_name = 'server_name';
 UPDATE phpbb_config SET config_value = 'https://next.turbolab.it' WHERE config_value = 'https://turbolab.it';
+
+UPDATE phpbb_config SET config_value = 'localhost' WHERE config_name = 'smtp_host';
+UPDATE phpbb_config SET config_value = 25 WHERE config_name = 'smtp_port';
+UPDATE phpbb_config SET config_value = '' WHERE config_name = 'smtp_username';
+UPDATE phpbb_config SET config_value = '' WHERE config_name = 'smtp_password';
+
 TRUNCATE phpbb_sessions_keys;
 TRUNCATE phpbb_sessions;
